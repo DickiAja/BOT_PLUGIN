812a0bc98f48bd9c6486fc12c4c1f69dceef5f46
dickiaja542 <dickiaja542@gmail.com>
2022-10-01 17:47:47 +0700

    Hentongers

---
+++ plugins/info-script.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*Nyari Esce ya :v*`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/fairuzzgapunyaduit",
+    mediaType: "VIDEO",
+    description: "yg bc kek kntl", 
+    title: 'FaiRuzz +62 - カムレン.',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
